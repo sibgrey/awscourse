41abede828c0005360e743575537beed202cc717
Aliaksei Kotsikau <mrtibon@gmail.com>
2023-04-14 16:31:51 +0200

    fail test

--- health_check.sh
+++ health_check.sh
@@ -2,8 +2,9 @@
 
 while 'true'
 do
-    if [ "$(curl -s http://localhost:8080/health)" = 'Hello from EC2' ]
+    if [ "$(curl -s http://localhost:8080/health)" = 'Hello from EC1' ]
     then
+        echo "server is running!"
         exit 0
     else
         echo "check server is running?"
