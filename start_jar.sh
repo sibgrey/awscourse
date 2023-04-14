188b0cf0dfe0ce63b87ba3bdf509e1004b575bfb
Aliaksei Kotsikau <mrtibon@gmail.com>
2023-04-14 16:27:49 +0200

    add health check to deploy lifecycle

--- start_jar.sh
+++ start_jar.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 java -jar /tmp/FirstEC2.jar > /dev/null 2> /dev/null < /dev/null  &
+# run process in the background, redirect stdout, stderr, and stdin to /dev/null.
--- stop_jar.sh
+++ stop_jar.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ps | grep java
+pgrep java
