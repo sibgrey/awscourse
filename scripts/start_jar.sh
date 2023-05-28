966c7ceeedb7b3e0bd7eb04bcf68eafa2cf004fb
Pavel Markovsky <markovskypavel@MBP-Pavel.lan>
2023-05-28 18:43:35 +0300

    Remove syntax error

--- scripts/start_jar.sh
+++ scripts/start_jar.sh
@@ -3,7 +3,7 @@ if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 then
   # upload Cloudwatch configuration, that was uploaded to S3
   echo "Uploading Cloudwatch configuration..."
-  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/tmp/amazon-cloudwatch-agent.json)
+  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/tmp/amazon-cloudwatch-agent.json
   # run process in the background, redirect stdout, stderr to nowhere (/dev/null)
   echo "Trying to run application..."
   nohup java -jar -Dspring.profiles.active=dev /tmp/billing-service.jar &
