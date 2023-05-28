9e3e99111adfde6b0208a6a381c3101246d3bd3e
Pavel Markovsky <markovskypavel@MBP-Pavel.lan>
2023-05-28 22:24:50 +0300

    Fix start jar

--- scripts/start_jar.sh
+++ scripts/start_jar.sh
@@ -4,7 +4,7 @@ then
   # upload Cloudwatch configuration, that was uploaded to S3
   echo "Uploading Cloudwatch configuration..."
   sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/tmp/amazon-cloudwatch-agent.json
-  # run process in the background, redirect stdin, stdout, stderr to nowhere (/dev/null)
+  # run process in the background, redirect stdin, stdout, stderr to nowhere (/dev/null). nohup redirects input and output to specific file nohup.out
   echo "Trying to run application..."
   nohup java -jar -Dspring.profiles.active=dev /tmp/billing-service.jar > /dev/null 2> /dev/null < /dev/null &
 else
