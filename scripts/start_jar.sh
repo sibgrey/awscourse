c2594f67850820072f12125196fc07d9afb7b6f3
Pavel Markovsky <markovskypavel@MBP-Pavel.lan>
2023-05-28 18:35:25 +0300

    Add overwrite option

--- scripts/start_jar.sh
+++ scripts/start_jar.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 then
-  # upload Cloudwatch configuration, that was uploaded to S3 (if you need to change location to file add: -c file:/tmp/amazon-cloudwatch-agent.json)
+  # upload Cloudwatch configuration, that was uploaded to S3
   echo "Uploading Cloudwatch configuration..."
-  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s
+  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/tmp/amazon-cloudwatch-agent.json)
   # run process in the background, redirect stdout, stderr to nowhere (/dev/null)
   echo "Trying to run application..."
   nohup java -jar -Dspring.profiles.active=dev /tmp/billing-service.jar &
