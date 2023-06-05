c41bb8c7029dded89e90e7049b0aa1b120e52904
Aliaksei Kotsikau <mrtibon@gmail.com>
2023-06-05 18:58:25 +0200

    fix start jar script

--- scripts/start_jar.sh
+++ scripts/start_jar.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
+
+# upload Cloudwatch configuration, that was uploaded to S3
+echo "Uploading Cloudwatch configuration..."
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/tmp/amazon-cloudwatch-agent.json
+# run process in the background, redirect stdin, stdout, stderr to nowhere (/dev/null). nohup redirects input and output to specific file nohup.out
+echo "Trying to run application..."
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "billing" ]
 then
-  # upload Cloudwatch configuration, that was uploaded to S3
-  echo "Uploading Cloudwatch configuration..."
-  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/tmp/amazon-cloudwatch-agent.json
-  # run process in the background, redirect stdin, stdout, stderr to nowhere (/dev/null). nohup redirects input and output to specific file nohup.out
-  echo "Trying to run application..."
-  if [ "$APPLICATION_NAME" == "billing" ]; then
-      nohup java -jar -Dspring.profiles.active=dev /tmp/billing-service.jar > /dev/null 2> /dev/null < /dev/null &
-      else nohup java -jar -Dspring.profiles.active=dev /tmp/order-service.jar > /dev/null 2> /dev/null < /dev/null &
-  fi
+    nohup java -jar -Dspring.profiles.active=dev /tmp/billing-service.jar > /dev/null 2> /dev/null < /dev/null &
 else
-  echo "No running applications was found for the deployment group name!"
+    nohup java -jar -Dspring.profiles.active=dev /tmp/order-service.jar > /dev/null 2> /dev/null < /dev/null &
 fi
+#else
+#  echo "No running applications was found for the deployment group name!"
+#fi
+
 exit 0
