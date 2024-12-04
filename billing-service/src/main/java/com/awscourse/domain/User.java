5be5a2344182568d8583afbabceb33ae16b40873
sibgrey <sbeganski@gmail.com>
2024-12-04 17:58:40 +0200

    pom update

--- billing-service/src/main/java/com/awscourse/domain/User.java
+++ billing-service/src/main/java/com/awscourse/domain/User.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
@@ -26,7 +27,7 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
